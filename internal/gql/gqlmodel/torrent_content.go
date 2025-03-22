a59c3962e49754c039672cebc6269e1c8df69b22
Alberto Leal <mail4alberto@gmail.com>
2025-03-22 19:25:32 -0400

    feat: Torrent size filter

--- internal/gql/gqlmodel/torrent_content.go
+++ internal/gql/gqlmodel/torrent_content.go
@@ -141,7 +141,29 @@ func (t TorrentContentQuery) Search(
 
 	if input.Facets != nil {
 		options = append(options, torrentContentFacetsOption(*input.Facets))
+
+		// Handle size range filters
+		if sizeRange, ok := input.Facets.SizeRange.ValueOK(); ok {
+			sizeCriteria := search.SizeRangeCriteria{
+				Key: "torrent_contents.size",
+			}
+
+			if min, minOk := sizeRange.Min.ValueOK(); minOk {
+				minSize := int64(*min)
+				sizeCriteria.MinBytes = &minSize
+			}
+
+			if max, maxOk := sizeRange.Max.ValueOK(); maxOk {
+				maxSize := int64(*max)
+				sizeCriteria.MaxBytes = &maxSize
+			}
+
+			if sizeCriteria.MinBytes != nil || sizeCriteria.MaxBytes != nil {
+				options = append(options, q.Where(sizeCriteria))
+			}
+		}
 	}
+
 
 	if infoHashes, ok := input.InfoHashes.ValueOK(); ok {
 		options = append(options, q.Where(search.TorrentContentInfoHashCriteria(infoHashes...)))
